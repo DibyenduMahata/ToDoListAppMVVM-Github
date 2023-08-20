317f14b81d73a50a8052dfd9a9445baa13b13481
dibyendu-mahata <dibyendumahata2016@gmail.com>
2023-08-20 17:01:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/todolistappmvvm/data/ToDoRepoImpl.kt
@@ -0,0 +1,24 @@
+package com.example.todolistappmvvm.data
+
+import kotlinx.coroutines.flow.Flow
+
+class ToDoRepositoryImpl(
+    private val dao: TodoDao
+): ToDoRepository {
+
+    override suspend fun insertTodo(todo: Todo) {
+     dao.insertTodo(todo)
+    }
+
+    override suspend fun deleteTodo(todo: Todo) {
+       dao.deleteTodo(todo)
+    }
+
+    override suspend fun getTodoById(id: Int): Todo? {
+       return dao.getTodoById(id)
+    }
+
+    override fun getTodos(): Flow<List<Todo>> {
+       return dao.getTodos()
+    }
+}
